7488bb3aee922ccd713d5790603fb63c290ac150
241105 <241105@std.hi-joho.ac.jp>
2025-06-20 12:29:48 +0900

    課題

---
+++ src/Character.java
@@ -0,0 +1,13 @@
+public abstract class Character implements Creature{
+    String name;
+    int hp;
+    public abstract void attack(Character target);
+
+    public final boolean isAlive(){
+        return hp > 0;
+    }
+
+    public void showStatus(){
+        System.out.println(name + " : HP " + hp);
+    }
+}
---
+++ src/GameMaster.java
@@ -0,0 +1,24 @@
+public class GameMaster {
+    public static void main(String[] args) {
+        Hero h = new Hero("アベル",100,"銅の剣");
+        Slime s = new Slime("スライムA",30);
+        System.out.println("冒険が始まる…！");
+        h.showStatus();
+        s.showStatus();
+        System.out.println("戦闘開始！");
+        h.attack(s);
+        s.showStatus();
+        s.attack(h);
+        h.showStatus();
+        h.heal();
+        h.showStatus();
+        h.attack(s);
+        h.attack(s);
+        h.attack(s);
+        System.out.println("--- 戦闘終了 ---\n最終ステータス:");
+        h.showStatus();
+        s.showStatus();
+        h.isAlive();
+        s.isAlive();
+    }
+}
