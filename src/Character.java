c041b61416ed7e73f452001c70bba93b36a0435c
241105 <241105@std.hi-joho.ac.jp>
2025-06-23 11:05:44 +0900

    課題

--- src/Character.java
+++ src/Character.java
@@ -1,7 +1,6 @@
 public abstract class Character implements Creature{
     String name;
     int hp;
-    public abstract void attack(Character target);
 
     public final boolean isAlive(){
         return hp > 0;
